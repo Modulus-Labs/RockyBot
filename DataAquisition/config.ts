d14d1d7a65e15bb1ea8030b15a53f1c5a497aba3
altanapps <futureisaltan@gmail.com>
2023-12-17 13:14:46 +0000

    fix: removes the API key in the Github repository and replaces that with appropriate README.md for reproducibility

--- DataAquisition/config.ts
+++ DataAquisition/config.ts
@@ -1,7 +1,7 @@
 var config = {
-    API_KEY: "1a5831e11ae8fdb2dddb6a42d98ca5a2",
-    //API_URL: `https://gateway.thegraph.com/api/1a5831e11ae8fdb2dddb6a42d98ca5a2/subgraphs/id/QmZeCuoZeadgHkGwLwMeguyqUKz1WPWQYKcKyMCeQqGhsF`,
-    API_URL: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
-}
+  API_KEY: "YOUR_API_KEY_HERE",
+  //API_URL: `https://gateway.thegraph.com/api/1a5831e11ae8fdb2dddb6a42d98ca5a2/subgraphs/id/QmZeCuoZeadgHkGwLwMeguyqUKz1WPWQYKcKyMCeQqGhsF`,
+  API_URL: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
+};
 
 export default config;
